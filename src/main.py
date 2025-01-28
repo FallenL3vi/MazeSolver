8bfacd649d1de89219211c0c59f51a386e4106e0
FallenL3vi <seba12351212@gmail.com>
2025-01-28 21:56:28 +0100

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from window import Window
 from line import Line
 from point import Point
+from cell import Cell
 
 def  main():
     win = Window(800, 600)
 
-    line_1 = Line(Point(0, 30), Point(100, 100))
-    line_2 = Line(Point(100, 100), Point(200, 0))
-    win.draw_line(line_1, "red")
-    win.draw_line(line_2, "red")
+    c = Cell(win)
+    c.draw(Point(100, 100), Point(150, 150))
     win.wait_for_close()
 
 main()
